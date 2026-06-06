fc023fdbe55808b6e1712daae92a64a8e2918bd2
Sakshi Joshi <78169779+sj2902@users.noreply.github.com>
2026-06-06 18:11:45 +0530

    Update task1_3.py

--- src/task1_3.py
+++ src/task1_3.py
@@ -13,12 +13,8 @@ unique_grocery_items_notNull = combined_col.filter(col('product').isNotNull())
 
 top_products_df = unique_grocery_items_notNull.groupBy('product').count().orderBy(desc('count')).limit(5)
 
-
-# 4. Format rows to match the required tuple layout: ('product_name', count)
-# To create an open bracket line, item line, and close bracket line, we collect them cleanly.
 top_5_records = top_products_df.collect()
 
-# 5. Write out the results as a normal text file using the specific layout
 output_filename  = "C:/Users/Sakshi/PycharmProjects/PythonProject/OSN/out/out_1_3.txt"
 
 with open(output_filename, "w", encoding="utf-8") as file:
@@ -27,7 +23,4 @@ with open(output_filename, "w", encoding="utf-8") as file:
         product_name = row["product"]
         frequency = row["count"]
 
-        # Match your exact structural spacing and bracket layout
         file.write(f"('{product_name}', {frequency})\n")
-
-print(f"Successfully generated top products file at: {output_filename}")
