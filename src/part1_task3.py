882f016010d12cfad106ad21bc4c6f4403d6873d
Sakshi Joshi <78169779+sj2902@users.noreply.github.com>
2026-06-07 17:37:40 +0530

    renaming part1_task3.py file

--- src/part1_task3.py
+++ src/part1_task3.py
@@ -1,7 +1,8 @@
+#Part 1(Spark API) Task 3
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import explode, array, col, desc
 
-spark = SparkSession.builder.appName('Top5').getOrCreate()
+spark = SparkSession.builder.appName('part1_task3').getOrCreate()
 
 df = spark.read.csv(r'C:\Users\Sakshi\PycharmProjects\PythonProject\OSN\raw_data\groceries.csv', header=False, inferSchema=True)
 
