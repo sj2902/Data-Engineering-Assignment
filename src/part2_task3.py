be872b6f5a9b356cfef9afaa07bc3a5bb37848b4
Sakshi Joshi <78169779+sj2902@users.noreply.github.com>
2026-06-07 17:51:36 +0530

    changes

--- src/part2_task3.py
+++ src/part2_task3.py
@@ -2,7 +2,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.appName('part2').getOrCreate()
+spark = SparkSession.builder.appName('part2_task3').getOrCreate()
 
 airbnb_df = spark.read.format('parquet').option('header',True).option('inferSchema',True).load(r'C:\Users\Sakshi\PycharmProjects\PythonProject\OSN\raw_data\part2\part-00000-tid-4320459746949313749-5c3d407c-c844-4016-97ad-2edec446aa62-6688-1-c000.snappy.parquet')
 
@@ -16,4 +16,4 @@ avg_df = filtered_df.agg(
 avg_df.coalesce(1).write\
     .mode('overwrite')\
     .option('header', True)\
-    .csv("C:/Users/Sakshi/PycharmProjects/PythonProject/OSN/out/out_2_3.txt")
+    .csv("C:/Users/Sakshi/PycharmProjects/PythonProject/OSN/out/out_2_3")
