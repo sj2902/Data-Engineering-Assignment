bb3896cc0a71467f3428bcbe47db0ad39c0dfb4a
Sakshi Joshi <78169779+sj2902@users.noreply.github.com>
2026-06-07 17:53:05 +0530

    Refactor task 4 code for clarity and organization

--- src/part2_task4.py
+++ src/part2_task4.py
@@ -1,12 +1,10 @@
+#Part 2 (Spark Dataframe API) Task 4
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.appName('part2').getOrCreate()
+spark = SparkSession.builder.appName('part2_task4').getOrCreate()
 
 airbnb_df = spark.read.format('parquet').option('header',True).option('inferSchema',True).load(r'C:\Users\Sakshi\PycharmProjects\PythonProject\OSN\raw_data\part2\part-00000-tid-4320459746949313749-5c3d407c-c844-4016-97ad-2edec446aa62-6688-1-c000.snappy.parquet')
-
-# columns = airbnb_df.columns
-# print(columns)
 
 ordered_df = airbnb_df.orderBy(col('price').asc(), col('review_scores_rating').desc())
 
