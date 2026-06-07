f2b23478d98422ca50c3455eae95f5479028987c
Sakshi Joshi <78169779+sj2902@users.noreply.github.com>
2026-06-07 17:51:07 +0530

    changes

--- src/part2_task2.py
+++ src/part2_task2.py
@@ -2,7 +2,7 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 
-spark = SparkSession.builder.appName('part2').getOrCreate()
+spark = SparkSession.builder.appName('part2_task2').getOrCreate()
 
 airbnb_df = spark.read.format('parquet').option('header',True).option('inferSchema',True).load(r'C:\Users\Sakshi\PycharmProjects\PythonProject\OSN\raw_data\part2\part-00000-tid-4320459746949313749-5c3d407c-c844-4016-97ad-2edec446aa62-6688-1-c000.snappy.parquet')
 
