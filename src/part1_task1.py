b23572f5b54fc76ac066ec493b902e0d0db6104a
Sakshi Joshi <78169779+sj2902@users.noreply.github.com>
2026-06-07 17:24:50 +0530

    Update and rename task1_1.py to part1_task1.py

--- src/part1_task1.py
+++ src/part1_task1.py
@@ -1,7 +1,7 @@
+#Part 1(Spark API) Task 1
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import explode, array, col
 
-spark = SparkSession.builder.appName('task1_1').getOrCreate()
+spark = SparkSession.builder.appName('part1_task1').getOrCreate()
 
 df = spark.read.csv(r'C:\Users\Sakshi\PycharmProjects\PythonProject\OSN\raw_data\groceries.csv', header=False, inferSchema=True)
 
